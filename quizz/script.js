83d6ef33f464b15711306d9dbe1fbfad89924a92
Guillaume FLOCH <g070786@gmail.com>
2015-12-01 12:05:09 +0100

    fix tiragequestion

--- quizz/script.js
+++ quizz/script.js
@@ -1,42 +1,61 @@
-// nQ = Numéro question
-// Q = Question
-// R = Réponse réelle
-// r = Propositions
+/**
+ * Created by valentin on 26/11/15.
+ */
 
-$(document).ready(function () {
-
-});
-
+    // nQ = Numéro question
+    // Q = Question
+    // R = Réponse réelle
+    // r = Propositions
 var mettage = $('.quizz');
-
-$.getJSON('./tab.json', function (json) {
+var nbrQ;
+$.getJSON('./question.json',function(json){
     nbrQ = Object.keys(json).length;
 });
 
-function nombreAlea(min, max) {
+function nombreAlea(min, max){
     return Math.floor(Math.random() * max + min);
 }
 
-function getQuestion(num) {
-    $.getJSON('./tab.json', function (json) {
-        $('<p class="question">' + json[num].Q + '</p>').appendTo(mettage);
+function getQuestion(num){
+    $.getJSON('./question.json', function(json){
+        $('<p class="question">'+ json[num].Q +'</p>').appendTo(mettage);
     })
 }
 
-function getReponse(num) {
-    $.getJSON('./tab.json', function (json) {
+function getReponse(num){
+    $.getJSON('./question.json', function(json){
         var nbrReponses = Object.keys(json[num]['r']).length;
-        for (var e = 0; e < nbrReponses; e++) {
+        for (var e = 0; e < nbrReponses; e++){
             $('<input type="radio" name="' + json[num].nQ + '" value="' + json[num].nQ + json[num].r[e] + '"> ' + json[num].r[e] + '<br />').appendTo(mettage);
-            //  Ajout d'un numero d'identification pour la question
+                                                                    //  ^ Rajout d'un identifiant sous forme de chiffre arabe unitaire non décimale propre à la question pour éviter eventuelle bug avec d'autre questions
         }
     })
 }
-function tirageQuestion() {
+function getPoint(num, callback){
+    $.getJSON('./question.json', function(json){
+        callback(json[num].Sc)
+    })
+}
+function checkR(num, callback){
+    $.getJSON('./question.json', function(json){
+        var rU = $('input[name="' + num + '"]:checked').val();
+
+        if (rU == json[num].nQ + json[num].R){
+
+            callback(true)
+
+        }
+        else{
+           callback(false)
+        }
+    });
+}
+
+function tirageQuestion(){
     return nombreAlea(1, nbrQ);
 }
 
-function getQR(num) {
+function getQR(num){
     mettage.empty();
     getQuestion(num);
     getReponse(num);
